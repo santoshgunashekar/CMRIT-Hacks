823c32d97d5d152ffbf5f051543734bc231b22b1
santoshguna001 <santoshguna001@gmail.com>
2018-05-06 11:20:55 +0530

    Intitial commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 400px;
+  height: 450px;
+  padding-top: 25px;
+  margin-left: 20px;
+  margin-bottom: 10px;
+  
+  
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+
+.margin_of_card {
+  margin: 10px;
+  text-decoration: none;
+  color: inherit;
+}
+
+
+.image{
+  width: 380px;
+  align-content: center;
+
+}
